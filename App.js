ac945706d216088ae0b943cf6f22a6251d3592d3
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-01-17 03:13:03 +0300

    done calculator

---
+++ App.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll("button");
+const screenDisplay = document.querySelector(".screen");
+
+let calculation = [];
+
+let accumulativeCalculation;
+
+function calculate(button) {
+  const value = button.textContent;
+
+  if (value === "CLEAR") {
+    calculation = [];
+    screenDisplay.textContent = "0";
+  } else if (value === "=" ) {
+    screenDisplay.textContent = eval(accumulativeCalculation);
+  } else {
+    calculation.push(value);
+    accumulativeCalculation = calculation.join("");
+    screenDisplay.textContent = accumulativeCalculation;
+  }
+console.log(calculation);
+  
+}
+
+buttons.forEach((button) =>
+  button.addEventListener("click", () => calculate(button))
+);
